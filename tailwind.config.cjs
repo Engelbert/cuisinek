6d0713382d4f4a92c2b320ccce32c7f79a5afc01
Bert <Engelbert@users.noreply.github.com>
2022-03-01 20:43:19 +0100

    test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,4 +3,18 @@ module.exports = {
         './public/**/*.html',
         './src/**/*.{css,astro,js,jsx,svelte,ts,tsx,vue}',
     ],
+    purge: [],
+    theme: {
+        fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+        display: ['Nunito', 'sans-serif'],
+        body: ['Nunito', 'sans-serif']
+        },
+        extend: {}
+    },
+    variants: {},
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer')
+    ]
 };
